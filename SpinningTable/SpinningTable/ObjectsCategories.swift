f08ae07ce9a24305a4a3bf687ccd35bc3c8fddf6
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-08-08 13:57:05 -0700

    Category bit masks

---
+++ SpinningTable/SpinningTable/ObjectsCategories.swift
@@ -0,0 +1,14 @@
+//
+//  ObjectsCategories.swift
+//  SpinningTable
+//
+//  Created by Sally Ouyang on 2014-08-06.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+import Foundation
+
+let playerCategory:UInt32      =  0x1 << 0;
+let shurikenCategory:UInt32    =  0x1 << 1;
+let foodCategory:UInt32        =  0x1 << 2;
+let lifeCategory:UInt32        =  0x1 << 3;
