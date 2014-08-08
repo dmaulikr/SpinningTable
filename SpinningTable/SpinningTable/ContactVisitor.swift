e78bae8b5fcbeb6e91458c0c613daf4deabd67ab
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-08-08 14:45:43 -0700

    Double dispatch method changed

--- SpinningTable/SpinningTable/ContactVisitor.swift
+++ SpinningTable/SpinningTable/ContactVisitor.swift
@@ -16,40 +16,40 @@ class ContactVisitor:NSObject
     let contact:SKPhysicsContact!
     
     //first part of the double dispatch, an instance of a class which is named after one of the bodies in the contact
-    class func contactVisitorWithBody(body:SKPhysicsBody,forContact contact:SKPhysicsContact)->ContactVisitor!
+    class func touchBody(touch:SKPhysicsBody,forContact contact:SKPhysicsContact)->ContactVisitor!
     {
-        if body.categoryBitMask & playerCategory != 0 {
-            return PlayerContactVisitor(body: body, forContact: contact)
+        if touch.categoryBitMask & playerCategory != 0 {
+            return PlayerContactVisitor(touch: touch, forContact: contact)
         }
-        else if body.categoryBitMask & shurikenCategory != 0 {
-            return ShurikenContactVisitor(body: body, forContact: contact)
+        else if touch.categoryBitMask & shurikenCategory != 0 {
+            return ShurikenContactVisitor(touch: touch, forContact: contact)
         }
-        else if body.categoryBitMask & foodCategory != 0 {
-            return FoodContactVisitor(body: body, forContact: contact)
+        else if touch.categoryBitMask & foodCategory != 0 {
+            return FoodContactVisitor(touch: touch, forContact: contact)
         }
-        else if body.categoryBitMask & lifeCategory != 0 {
-            return LifeContactVisitor(body: body, forContact: contact)
+        else if touch.categoryBitMask & lifeCategory != 0 {
+            return LifeContactVisitor(touch: touch, forContact: contact)
         }
         return nil
     }
     
-    init(body:SKPhysicsBody, forContact contact:SKPhysicsContact)
+    init(touch:SKPhysicsBody, forContact contact:SKPhysicsContact)
     {
-        self.body = body
+        self.body = touch
         self.contact = contact
         super.init()
     }
     
     //second part of the double dispatch by sending a message named after the second physics body in the contact to the newly constructed ContactVisitor subclass instance which is named after the first body in the contact
-    func visitBody(body:SKPhysicsBody)
+    func touch(visitor:SKPhysicsBody)
     {
-        var contactSelectorString = "visit" + body.node.name + ":"
-        let selector = NSSelectorFromString(contactSelectorString)
-        if self.respondsToSelector(selector)
+        var visit = "visit" + visitor.node.name + ":"
+        let string1 = NSSelectorFromString(visit)
+        if self.respondsToSelector(string1)
         {
             dispatch_after(0, dispatch_get_main_queue(),
                 {
-                    NSThread.detachNewThreadSelector(selector, toTarget:self, withObject: body)
+                    NSThread.detachNewThreadSelector(string1, toTarget:self, withObject: visitor)
                 })
         }
     }
