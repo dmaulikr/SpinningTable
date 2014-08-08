1c14cea328e70c1a21ba3fe58d3b5a742944214a
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-08-08 14:00:34 -0700

    when these collide, do nothing

---
+++ SpinningTable/SpinningTable/ShurikenContactVisitor.swift
@@ -0,0 +1,38 @@
+//
+//  ShurikenContactVisitor.swift
+//  SpinningTable
+//
+//  Created by Sally Ouyang on 2014-08-06.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+//nothing to do here really....
+class ShurikenContactVisitor:ContactVisitor
+{
+    func visitPlayer(body:SKPhysicsBody)
+    {
+        let first = self.body.node
+        let second = body.node
+    }
+    
+    func visitShuriken(body:SKPhysicsBody)
+    {
+        let first = self.body.node
+        let second = body.node
+    }
+    
+    func visitFood(body:SKPhysicsBody)
+    {
+        let first = self.body.node
+        let second = body.node
+    }
+    
+    func visitLife(body:SKPhysicsBody)
+    {
+        let first = self.body.node
+        let second = body.node
+    }
+}
