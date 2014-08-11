ac35467db2fc7891d3ccbf2f79565aebfcf9a965
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-08-11 16:51:13 -0700

    Food aka the stars

---
+++ SpinningTable/SpinningTable/Food.swift
@@ -0,0 +1,19 @@
+//
+//  Food.swift
+//  SpinningTable
+//
+//  Created by Sally Ouyang on 2014-08-11.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+import SpriteKit
+
+class Food: Object
+{
+    convenience init()
+    {
+        self.init(name:"Food",image:"food")
+        self.physicsBody.categoryBitMask = foodCategory
+    }
+}
+
