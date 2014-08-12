be542e7be711d92e501f35eab9452b2c1c2984ce
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-08-11 17:05:43 -0700

    the life

---
+++ SpinningTable/SpinningTable/Life.swift
@@ -0,0 +1,18 @@
+//
+//  Life.swift
+//  SpinningTable
+//
+//  Created by Sally Ouyang on 2014-08-11.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+import SpriteKit
+
+class Life: Object
+{
+    convenience init()
+    {
+        self.init(name:"Life",image:"life")
+        self.physicsBody.categoryBitMask = lifeCategory
+    }
+}
