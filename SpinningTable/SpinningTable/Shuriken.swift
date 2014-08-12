69be58271994201b32fe05d739a4eff4ef88f943
Sally Ouyang <sally.ouyang@pulseenergy.com>
2014-08-11 17:06:19 -0700

    The weapon

---
+++ SpinningTable/SpinningTable/Shuriken.swift
@@ -0,0 +1,18 @@
+//
+//  Shuriken.swift
+//  SpinningTable
+//
+//  Created by Sally Ouyang on 2014-08-11.
+//  Copyright (c) 2014 Sally Ouyang. All rights reserved.
+//
+
+import SpriteKit
+
+class Shuriken: Object
+{
+    convenience init()
+    {
+        self.init(name:"Shuriken",image:"shuriken")
+        self.physicsBody.categoryBitMask = shurikenCategory
+    }
+}
